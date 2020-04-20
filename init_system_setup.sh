835ae804ccb1889784aef71c4437a90e3cba6dcd
Jonathan Berling <jberling@nvidia.com>
2020-04-20 10:32:05 -0700

    Add inital system setup script

---
+++ init_system_setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install -y \
+clang gcc g++ \
+git cmake \
+htop tree vim \
+wget curl \
+python python3 python3-pip \
+zsh \
+
+
