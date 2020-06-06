1354cccf94b2d23820aa53ce252d642fce03ee4b
Jonathan Berling <jonberling@gmail.com>
2020-06-06 15:55:15 -0700

    Update init system setup script

--- init_system_setup.sh
+++ init_system_setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# update and upgrade system
 sudo apt update -y
 sudo apt upgrade -y
 
+# install common packages
 sudo apt install -y \
 clang gcc g++ \
 git cmake \
@@ -11,4 +13,8 @@ wget curl \
 python python3 python3-pip \
 zsh \
 
+# install Oh My Zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
+# install openconnect
+# sudo apt-get install network-manager-openconnect network-manager-openconnect-gnome
